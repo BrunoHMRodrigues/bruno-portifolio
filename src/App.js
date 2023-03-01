c132758ae10c36628170f7f642715caaa5aebed8
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-03-01 18:10:39 -0300

    Feature> Page contact

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import VisitorLogin from './pages/VisitorLogin';
 import Home from './pages/Home';
 import AboutMe from './pages/AboutMe';
 import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <div>
       <Switch>
         <Route exact path="/" component={VisitorLogin} />
         <Route path="/home" component={Home} />
         <Route path="/aboutme" component={AboutMe} />
         <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
       </Switch>
     </div>
     </div>
